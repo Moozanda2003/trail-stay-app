db53a9c39bb6ee61f7a7da86394329d1ef9f6ced
Moozanda2003 <chris.r.sedgwick@gmail.com>
2025-07-30 22:10:47 -0700

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Home = () => {
-  return <div className="p-4">Home Page</div>;
+const Home = ({ filters, favorites, toggleFavorite }) => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Welcome to Trail Stay</h1>
+      <p>This is the home page. Your filters will apply here soon.</p>
+    </div>
+  );
 };
 
 export default Home;
